ce20efd8092e3f9f6846a78a12bb7e6667895436
Erincz Arnold <arnold.erincz@gmail.com>
2020-09-19 22:41:51 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'dlonra',
+      database : 'face_det'
+    }
+});
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+let cur_id = 1;
+
+const database = {
+    users:[
+        {
+            id: cur_id++,
+            name: 'John',
+            email:'john@gmail.com',
+            password: 'banana',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) =>{
+    let found = false;
+    database.users.forEach(user => {
+        if((req.body.email === user.email) && (req.body.password === user.password)){
+            found = true;
+            return res.json(user)
+        } 
+    })
+    if(!found){
+        res.json("Can't find user")
+    }
+})
+
+app.post('/register', (req, res) =>{
+    const {name, email, pass} = req.body;
+    db('users').returning('*')
+    .insert({
+        name: name,
+        email: email,
+        joined: new Date()
+        })
+        .then(user => res.json(user[0]))
+        .catch(err => res.status(400).json('Unable to join'));
+    
+})
+
+app.get('/profile/:id', (req, res) =>{
+    const {id} = req.params;
+    db.select('*').from('users').where({id: id}).then(user =>{
+        if(user.length){
+            res.json(user[0])
+        }
+        else{
+            res.status(400).json("User not found");
+        }
+    })
+})
+
+app.put('/image', (req, res) => {
+    let found = false;
+    const {id} = req.body;
+    database.users.forEach(user =>{
+        if(Number(user.id)=== Number(id)){
+            user.entries++;
+            found = true;
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json("User not found");
+    }
+})
+
+app.listen(3001, () =>{
+console.log('app is running on port 3001')
+});
+
+/*
+/signin --> POST = succes/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user updated
+
+*/
